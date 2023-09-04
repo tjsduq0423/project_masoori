37297f2f709f48c8c54b2cf2850c0da7bcef6a21
문준호 <tjsduq0423@naver.com>
2023-09-04 14:11:27 +0900

    [S09P22B308-11] feat :  기본 프로젝트 구조 및 User 로그인 관련 코드 베이스 작성

---
+++ backend/src/main/java/com/fintech/masoori/global/error/exception/EntityNotFoundException.java
@@ -0,0 +1,10 @@
+package com.fintech.masoori.global.error.exception;
+
+import com.fintech.masoori.global.error.ErrorCode;
+
+public class EntityNotFoundException extends BusinessException {
+
+	public EntityNotFoundException(String message) {
+		super(message, ErrorCode.ENTITY_NOT_FOUND);
+	}
+}
