2c540909b9b693c8a603eb8385d1bdf1ea2318e1
문준호 <tjsduq0423@naver.com>
2023-09-12 16:37:07 +0900

    [S09P22B308] feat : CI/CD

--- start-frontend.sh
+++
@@ -1,5 +0,0 @@
-docker compose -f docker-compose-front.yml pull
-
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker compose -f docker-compose-front.yml up --build -d
-
-docker rmi -f $(docker images -f "dangling=true" -q) || true
