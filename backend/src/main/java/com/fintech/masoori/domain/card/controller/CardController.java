2255a1dd36da2b6e06ebbd13591f863ce727a978
황준혁 <hyug0819@gmail.com>
2023-09-13 17:51:54 +0900

    [S09P22B308-100] feat : Card Controller Swagger 적용

--- backend/src/main/java/com/fintech/masoori/domain/card/controller/CardController.java
+++ backend/src/main/java/com/fintech/masoori/domain/card/controller/CardController.java
@@ -1,6 +1,7 @@
 package com.fintech.masoori.domain.card.controller;
 
-import java.sql.Timestamp;
+import java.security.Principal;
+import java.time.LocalDateTime;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,44 +11,64 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fintech.masoori.domain.card.dto.BasicCardRes;
+import com.fintech.masoori.domain.card.dto.ChallengeCardRes;
+import com.fintech.masoori.domain.card.dto.SelectBasicCardReq;
+import com.fintech.masoori.domain.card.dto.SelectChallengeCardReq;
 import com.fintech.masoori.domain.card.service.CardService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/card")
 @RequiredArgsConstructor
+@Tag(name = "Card", description = "소비카드 및 챌린지카드 API")
 public class CardController {
 	private final CardService cardService;
 
 	//소비 카드 생성
+	@Operation(summary = "소비 카드 생성 API", description = "사용자 최초 등록 시 소비카드를 생성한다.")
 	@PostMapping("/consume")
-	public ResponseEntity<?> createConsumeCard() {
-		return ResponseEntity.ok(200);
+	public ResponseEntity<?> createConsumeCard(Principal principal) {
+		return ResponseEntity.ok().build();
 	}
 
 	//소비 카드 조회
+	@Operation(summary = "소비 카드 조회 API", description = "유저의 모든 소비카드 조회, 연,월을 기준으로 페이지네이션")
 	@GetMapping("/consume")
-	public ResponseEntity<?> selectConsumeCard() {
-		return ResponseEntity.ok(200);
+	public ResponseEntity<BasicCardRes.BasicCard> selectConsumeCard(
+		@Parameter(description = "조회 시작일, 조회 개수")
+		@RequestBody SelectBasicCardReq basicCardReq, Principal principal) {
+		return ResponseEntity.ok().build();
 	}
 
 	//소비 카드 상세 조회
+	@Operation(summary = "소비 카드 상세 조회 API", description = "소비카드 한 장을 조회한다.")
 	@GetMapping("/consume/{id}")
-	public ResponseEntity<?> detailConsumeCard(@PathVariable Long id) {
-		return ResponseEntity.ok(200);
+	public ResponseEntity<BasicCardRes> detailConsumeCard(
+		@Parameter(description = "소비카드 id", required = true, example = "1")
+		@PathVariable Long id, Principal principal) {
+		return ResponseEntity.ok().build();
 	}
 
 	//챌린지 조회
+	@Operation(summary = "챌린지 조회 API", description = "유저에게 할당되어 있는 챌린지카드와 챌린지를 연,월일을 통해 조회한다.")
 	@GetMapping("/challenge")
-	public ResponseEntity<?> selectChallenge(@RequestBody Timestamp time) {
-		return ResponseEntity.ok(200);
+	public ResponseEntity<ChallengeCardRes.ChallengeCard> selectChallenge(
+		@Parameter(description = "조회할 연,월", required = true, example = "202308")
+		@RequestBody LocalDateTime time, Principal principal) {
+		return ResponseEntity.ok().build();
 	}
 
 	//챌린지 카드 조회
-	@GetMapping("/challengecard/{id}")
-	public ResponseEntity<?> selectChallengeCard(@PathVariable Long id) {
-		return ResponseEntity.ok(200);
+	@Operation(summary = "챌린지 카드 조회 API", description = "조회 시작일과 종료일을 통해 유저의 챌린지 카드들을 조회")
+	@GetMapping("/challengecard")
+	public ResponseEntity<ChallengeCardRes> selectChallengeCard(
+		@RequestBody SelectChallengeCardReq challengeCardReq, Principal principal) {
+		return ResponseEntity.ok().build();
 	}
 
 }
