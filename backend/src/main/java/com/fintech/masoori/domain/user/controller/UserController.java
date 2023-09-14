7e01076b93811110471aeb899ba75061418b5833
송소연 <songsoy95@gmail.com>
2023-09-14 09:26:41 +0900

    [S09P12B102-143] feat : 휴대폰 인증 구현

--- backend/src/main/java/com/fintech/masoori/domain/user/controller/UserController.java
+++ backend/src/main/java/com/fintech/masoori/domain/user/controller/UserController.java
@@ -1,11 +1,5 @@
 package com.fintech.masoori.domain.user.controller;
 
-import java.util.Optional;
-
-import com.fintech.masoori.domain.user.dto.SmsCheckReq;
-import com.fintech.masoori.domain.user.exception.*;
-import jakarta.mail.MessagingException;
-import net.nurigo.java_sdk.exceptions.CoolsmsException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
@@ -18,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fintech.masoori.domain.user.dto.EmailCheckReq;
 import com.fintech.masoori.domain.user.dto.SendEmailReq;
 import com.fintech.masoori.domain.user.dto.SendSmsReq;
+import com.fintech.masoori.domain.user.dto.SmsCheckReq;
+import com.fintech.masoori.domain.user.entity.User;
 import com.fintech.masoori.domain.user.service.EmailService;
 import com.fintech.masoori.domain.user.service.SmsService;
-import com.fintech.masoori.domain.user.entity.User;
 import com.fintech.masoori.domain.user.service.UserService;
 import com.fintech.masoori.global.error.exception.InvalidValueException;
 import com.fintech.masoori.global.redis.RedisService;
@@ -37,82 +32,81 @@ import lombok.extern.slf4j.Slf4j;
 @Tag(name = "user", description = "회원 API")
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
-    private final EmailService emailService;
-    private final RedisService redisService;
-    private final SmsService smsService;
+	private final UserService userService;
+	private final EmailService emailService;
+	private final RedisService redisService;
+	private final SmsService smsService;
 
-    @Operation(summary = "회원가입 시 이메일 인증코드 발송 API")
-    @PostMapping("/email/signup")
-    public ResponseEntity<?> sendSignupEmailCode(
-            @Parameter(description = "회원 이메일", required = true)
-            @RequestBody @Validated SendEmailReq sendEmailReq, BindingResult bindingResult) {
-        validateRequest(bindingResult);
-        userService.sendSignupEmailCode(sendEmailReq.getEmail());
-        return ResponseEntity.ok().build();
-    }
+	@Operation(summary = "회원가입 시 이메일 인증코드 발송 API")
+	@PostMapping("/email/signup")
+	public ResponseEntity<?> sendSignupEmailCode(
+		@Parameter(description = "회원 이메일", required = true)
+		@RequestBody @Validated SendEmailReq sendEmailReq, BindingResult bindingResult) {
+		validateRequest(bindingResult);
+		userService.sendSignupEmailCode(sendEmailReq.getEmail());
+		return ResponseEntity.ok().build();
+	}
 
-    @Operation(summary = "비밀번호 재설정 시 이메일 인증코드 발송 API")
-    @PostMapping("/email/password")
-    public ResponseEntity<?> sendPasswordEmailCode(
-            @Parameter(description = "회원 이메일", required = true)
-            @RequestBody @Validated SendEmailReq sendEmailReq, BindingResult bindingResult) {
-        validateRequest(bindingResult);
-        userService.sendPasswordEmailCode(sendEmailReq.getEmail());
-        return ResponseEntity.ok().build();
-    }
+	@Operation(summary = "비밀번호 재설정 시 이메일 인증코드 발송 API")
+	@PostMapping("/email/password")
+	public ResponseEntity<?> sendPasswordEmailCode(
+		@Parameter(description = "회원 이메일", required = true)
+		@RequestBody @Validated SendEmailReq sendEmailReq, BindingResult bindingResult) {
+		validateRequest(bindingResult);
+		userService.sendPasswordEmailCode(sendEmailReq.getEmail());
+		return ResponseEntity.ok().build();
+	}
 
-    @Operation(summary = "이메일 인증코드 검증 API")
-    @PostMapping("/email/check")
-    public ResponseEntity<?> verifyEmailCode(
-            @Parameter(description = "회원 이메일, 인증 코드", required = true)
-            @RequestBody @Validated EmailCheckReq emailCheckReq, BindingResult bindingResult) {
-        validateRequest(bindingResult);
-        userService.verifyEmailCode(emailCheckReq);
-        return ResponseEntity.ok().build();
-    }
+	@Operation(summary = "이메일 인증코드 검증 API")
+	@PostMapping("/email/check")
+	public ResponseEntity<?> verifyEmailCode(
+		@Parameter(description = "회원 이메일, 인증 코드", required = true)
+		@RequestBody @Validated EmailCheckReq emailCheckReq, BindingResult bindingResult) {
+		validateRequest(bindingResult);
+		userService.verifyEmailCode(emailCheckReq);
+		return ResponseEntity.ok().build();
+	}
 
-//    @Operation(summary = "휴대폰 인증코드 발송 API")
-//    @PostMapping("/sms")
-//    public ResponseEntity<?> sendSms(
-//            @Parameter(description = "회원")
-//            @RequestBody @Validated SendSmsReq sendSmsReq, BindingResult bindingResult, Authentication authentication) {
-//        validateRequest(bindingResult);
-//        User loginUser = loginUser(authentication);
-//        userService.updateInfoAndSendSms(sendSmsReq, loginUser);
-//        return ResponseEntity.ok().build();
-//    }
+	//    @Operation(summary = "휴대폰 인증코드 발송 API")
+	//    @PostMapping("/sms")
+	//    public ResponseEntity<?> sendSms(
+	//            @Parameter(description = "회원")
+	//            @RequestBody @Validated SendSmsReq sendSmsReq, BindingResult bindingResult, Authentication authentication) {
+	//        validateRequest(bindingResult);
+	//        User loginUser = loginUser(authentication);
+	//        userService.updateInfoAndSendSms(sendSmsReq, loginUser);
+	//        return ResponseEntity.ok().build();
+	//    }
 
-    @Operation(summary = "휴대폰 인증코드 발송 API")
-    @PostMapping("/sms")
-    public ResponseEntity<?> sendSms(
-            @Parameter(description = "회원")
-            @RequestBody @Validated SendSmsReq sendSmsReq, BindingResult bindingResult) {
-        validateRequest(bindingResult);
-        User loginUser = userService.findByEmail(sendSmsReq.getEmail()).get();
-        userService.updateInfoAndSendSms(sendSmsReq, loginUser);
-        return ResponseEntity.ok().build();
-    }
+	@Operation(summary = "휴대폰 인증코드 발송 API")
+	@PostMapping("/sms")
+	public ResponseEntity<?> sendSms(
+		@Parameter(description = "회원")
+		@RequestBody @Validated SendSmsReq sendSmsReq, BindingResult bindingResult) {
+		validateRequest(bindingResult);
+		User loginUser = userService.findByEmail(sendSmsReq.getEmail()).get();
+		userService.updateInfoAndSendSms(sendSmsReq, loginUser);
+		return ResponseEntity.ok().build();
+	}
 
-    @Operation(summary = "휴대폰 인증코드 검증 API")
-    @PostMapping("/sms/check")
-    public ResponseEntity<?> verifySmsCode(
-            @Parameter(description = "회원 전화번호, 인증 코드", required = true)
-            @RequestBody @Validated SmsCheckReq smsCheckReq, BindingResult bindingResult) {
-        validateRequest(bindingResult);
-        userService.verifySmsCode(smsCheckReq);
-        return ResponseEntity.ok().build();
-    }
+	@Operation(summary = "휴대폰 인증코드 검증 API")
+	@PostMapping("/sms/check")
+	public ResponseEntity<?> verifySmsCode(
+		@Parameter(description = "회원 전화번호, 인증 코드", required = true)
+		@RequestBody @Validated SmsCheckReq smsCheckReq, BindingResult bindingResult) {
+		validateRequest(bindingResult);
+		userService.verifySmsCode(smsCheckReq);
+		return ResponseEntity.ok().build();
+	}
 
+	private void validateRequest(BindingResult bindingResult) {
+		if (bindingResult.hasErrors())
+			throw new InvalidValueException("Invalid Input Value");
+	}
 
-    private void validateRequest(BindingResult bindingResult) {
-        if (bindingResult.hasErrors())
-            throw new InvalidValueException("Invalid Input Value");
-    }
-
-    // 현재 로그인한 유저 정보를 찾아온다.
-    public User loginUser(Authentication authentication) {
-        return userService.findByEmail(authentication.getPrincipal().toString()).get();
-    }
+	// 현재 로그인한 유저 정보를 찾아온다.
+	public User loginUser(Authentication authentication) {
+		return userService.findByEmail(authentication.getPrincipal().toString()).get();
+	}
 
 }
--- backend/src/main/java/com/fintech/masoori/domain/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/fintech/masoori/domain/user/service/UserServiceImpl.java
@@ -2,18 +2,26 @@ package com.fintech.masoori.domain.user.service;
 
 import java.util.Optional;
 
-import com.fintech.masoori.domain.user.dto.*;
-import com.fintech.masoori.domain.user.exception.*;
-import com.fintech.masoori.global.redis.RedisService;
-import jakarta.mail.MessagingException;
-import net.nurigo.java_sdk.exceptions.CoolsmsException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.fintech.masoori.domain.user.entity.User;
-import com.fintech.masoori.domain.user.repository.UserRepository;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
+import com.fintech.masoori.domain.user.dto.EmailCheckReq;
+import com.fintech.masoori.domain.user.dto.LoginReq;
+import com.fintech.masoori.domain.user.dto.SendSmsReq;
+import com.fintech.masoori.domain.user.dto.SignUpReq;
+import com.fintech.masoori.domain.user.dto.SmsCheckReq;
+import com.fintech.masoori.domain.user.entity.User;
+import com.fintech.masoori.domain.user.exception.EmailDuplicationException;
+import com.fintech.masoori.domain.user.exception.EmailMessagingException;
+import com.fintech.masoori.domain.user.exception.InvalidAuthCodeException;
+import com.fintech.masoori.domain.user.exception.SmsMessagingException;
+import com.fintech.masoori.domain.user.exception.UserNotFoundException;
+import com.fintech.masoori.domain.user.repository.UserRepository;
+import com.fintech.masoori.global.redis.RedisService;
+
+import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -25,104 +33,103 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
-    private final RedisService redisService;
-    private final EmailService emailService;
-    private final SmsService smsService;
+	private final UserRepository userRepository;
+	private final RedisService redisService;
+	private final EmailService emailService;
+	private final SmsService smsService;
 
-    @Override
-    public boolean checkEmail(String email) throws Exception {
-        return false;
-    }
+	@Override
+	public boolean checkEmail(String email) throws Exception {
+		return false;
+	}
 
-    @Override
-    public boolean checkOAuthAccount(String email) {
-        return false;
-    }
+	@Override
+	public boolean checkOAuthAccount(String email) {
+		return false;
+	}
 
-    @Override
-    public void signUp(SignUpReq signUpReq) {
+	@Override
+	public void signUp(SignUpReq signUpReq) {
 
-    }
+	}
 
-    @Override
-    public void login(LoginReq loginReq) {
+	@Override
+	public void login(LoginReq loginReq) {
 
-    }
+	}
 
-    @Override
-    public void logout(HttpServletRequest request, HttpServletResponse response) {
+	@Override
+	public void logout(HttpServletRequest request, HttpServletResponse response) {
 
-    }
+	}
 
-    @Override
-    public Optional<User> findByEmail(String email) {
-        Optional<User> findUser = userRepository.findByEmail(email);
-        return findUser;
-    }
+	@Override
+	public Optional<User> findByEmail(String email) {
+		Optional<User> findUser = userRepository.findByEmail(email);
+		return findUser;
+	}
 
+	@Override
+	public void sendSignupEmailCode(String email) {
+		Optional<User> findUser = findByEmail(email);
+		// 사용자 존재 => 중복 => 가입 안됨
+		if (findUser.isPresent())
+			throw new EmailDuplicationException("Email is Duplicated");
+		sendEmail(email);
+	}
 
-    @Override
-    public void sendSignupEmailCode(String email) {
-        Optional<User> findUser = findByEmail(email);
-        // 사용자 존재 => 중복 => 가입 안됨
-        if (findUser.isPresent())
-            throw new EmailDuplicationException("Email is Duplicated");
-        sendEmail(email);
-    }
+	@Override
+	public void sendPasswordEmailCode(String email) {
+		Optional<User> findUser = findByEmail(email);
+		// 사용자 존재x => 없는 사용자
+		if (!findUser.isPresent())
+			throw new UserNotFoundException("User Not Found");
+		sendEmail(email);
+	}
 
-    @Override
-    public void sendPasswordEmailCode(String email) {
-        Optional<User> findUser = findByEmail(email);
-        // 사용자 존재x => 없는 사용자
-        if (!findUser.isPresent())
-            throw new UserNotFoundException("User Not Found");
-        sendEmail(email);
-    }
+	public void sendEmail(String email) {
+		// 인증코드 redis 서버에 저장
+		String code = emailService.createCode(8);
+		redisService.setEmailCode(email, code);
+		try {
+			emailService.sendEmail(email, code);
+		} catch (MessagingException e) {
+			redisService.deleteEmailCode(email);
+			throw new EmailMessagingException("Failed To Send Email");
+		}
+	}
 
-    public void sendEmail(String email) {
-        // 인증코드 redis 서버에 저장
-        String code = emailService.createCode(8);
-        redisService.setEmailCode(email, code);
-        try {
-            emailService.sendEmail(email, code);
-        } catch (MessagingException e) {
-            redisService.deleteEmailCode(email);
-            throw new EmailMessagingException("Failed To Send Email");
-        }
-    }
+	@Override
+	public void verifyEmailCode(EmailCheckReq emailCheckReq) {
+		String inputCode = emailCheckReq.getCode();
+		String savedCode = redisService.getEmailCode(emailCheckReq.getEmail());
+		if (!inputCode.equals(savedCode))
+			throw new InvalidAuthCodeException("Invalid Auth Code");
+	}
 
-    @Override
-    public void verifyEmailCode(EmailCheckReq emailCheckReq) {
-        String inputCode = emailCheckReq.getCode();
-        String savedCode = redisService.getEmailCode(emailCheckReq.getEmail());
-        if (!inputCode.equals(savedCode))
-            throw new InvalidAuthCodeException("Invalid Auth Code");
-    }
+	@Override
+	public void verifySmsCode(SmsCheckReq smsCheckReq) {
+		String inputCode = smsCheckReq.getCode();
+		String savedCode = redisService.getEmailCode(smsCheckReq.getPhoneNumber());
+		if (!inputCode.equals(savedCode))
+			throw new InvalidAuthCodeException("Invalid Auth Code");
+	}
 
-    @Override
-    public void verifySmsCode(SmsCheckReq smsCheckReq) {
-        String inputCode = smsCheckReq.getCode();
-        String savedCode = redisService.getEmailCode(smsCheckReq.getPhoneNumber());
-        if (!inputCode.equals(savedCode))
-            throw new InvalidAuthCodeException("Invalid Auth Code");
-    }
-
-    @Override
-    public void updateInfoAndSendSms(SendSmsReq sendSmsReq, User loginUser) {
-        // 사용자 정보 업데이트
-        String name = sendSmsReq.getName();
-        String phoneNumber = sendSmsReq.getPhoneNumber();
-        userRepository.updateInfo(loginUser.getEmail(), name, phoneNumber);
-        // 인증코드 redis 서버에 저장 후 메시지 발송
-        String code = smsService.createCode(6);
-        redisService.setSmsCode(phoneNumber, code);
-        try {
-            smsService.sendSms(phoneNumber, code);
-        } catch (CoolsmsException e) {
-            redisService.deleteSmsCode(phoneNumber);
-            throw new SmsMessagingException("Failed To Send Email");
-        }
-    }
+	@Override
+	public void updateInfoAndSendSms(SendSmsReq sendSmsReq, User loginUser) {
+		// 사용자 정보 업데이트
+		String name = sendSmsReq.getName();
+		String phoneNumber = sendSmsReq.getPhoneNumber();
+		userRepository.updateInfo(loginUser.getEmail(), name, phoneNumber);
+		// 인증코드 redis 서버에 저장 후 메시지 발송
+		String code = smsService.createCode(6);
+		redisService.setSmsCode(phoneNumber, code);
+		try {
+			smsService.sendSms(phoneNumber, code);
+		} catch (CoolsmsException e) {
+			redisService.deleteSmsCode(phoneNumber);
+			throw new SmsMessagingException("Failed To Send Email");
+		}
+	}
 
 }
