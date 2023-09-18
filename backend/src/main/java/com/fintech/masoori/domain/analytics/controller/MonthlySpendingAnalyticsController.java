154d5007856d85bb6be16c6f441b41d557d48109
황준혁 <hyug0819@gmail.com>
2023-09-19 00:20:02 +0900

    [S09P22B308-294] fix : Swagger Tag 변경

--- backend/src/main/java/com/fintech/masoori/domain/analytics/controller/MonthlySpendingAnalyticsController.java
+++ backend/src/main/java/com/fintech/masoori/domain/analytics/controller/MonthlySpendingAnalyticsController.java
@@ -17,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1/analytics")
 @RequiredArgsConstructor
-@Tag(name = "Card", description = "소비카드 및 챌린지카드 API")
+@Tag(name = "Analytics", description = "월간 소비 분석 API")
 public class MonthlySpendingAnalyticsController {
 	private final MonthlySpendingAnalyticsService monthlySpendingAnalyticsService;
 
