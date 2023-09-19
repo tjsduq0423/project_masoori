cb39929673cd22e68ad06ec701152546935a1e3c
송소연 <songsoy95@gmail.com>
2023-09-19 11:06:25 +0900

    [S09P12B308-320] fix : 유저 dto, 컨트롤러 수정

--- backend/src/main/java/com/fintech/masoori/domain/user/dto/SendSmsReq.java
+++ backend/src/main/java/com/fintech/masoori/domain/user/dto/SendSmsReq.java
@@ -1,5 +1,6 @@
 package com.fintech.masoori.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -7,9 +8,11 @@ import lombok.Data;
 public class SendSmsReq {
 
 	@NotBlank
+	@Schema(description = "사용자 이름", example = "홍길동")
 	private String name;
 
 	@NotBlank
+	@Schema(description = "사용자 휴대폰번호", example = "01012345678")
 	private String phoneNumber;
 
 }
