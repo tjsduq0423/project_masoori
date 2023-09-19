185c7d7cc3f899e61af06bdc6bc96a8636123294
황준혁 <hyug0819@gmail.com>
2023-09-19 17:35:09 +0900

    [S09P22B308-293] refactor : public 제거

--- backend/src/main/java/com/fintech/masoori/domain/lucky/repository/ColorRepository.java
+++ backend/src/main/java/com/fintech/masoori/domain/lucky/repository/ColorRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fintech.masoori.domain.lucky.entity.Color;
 
 public interface ColorRepository extends JpaRepository<Color, Long> {
-	public Color findDescriptionByColor(String color);
+	Color findDescriptionByColor(String color);
 }
--- backend/src/main/java/com/fintech/masoori/domain/lucky/service/ColorService.java
+++ backend/src/main/java/com/fintech/masoori/domain/lucky/service/ColorService.java
@@ -3,5 +3,5 @@ package com.fintech.masoori.domain.lucky.service;
 import com.fintech.masoori.domain.lucky.dto.ColorRes;
 
 public interface ColorService {
-	public ColorRes selectOneColor(String email);
+	ColorRes selectOneColor(String email);
 }
