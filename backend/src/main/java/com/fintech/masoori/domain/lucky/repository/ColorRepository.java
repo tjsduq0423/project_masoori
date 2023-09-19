b2f75309408da4779b40d7ad7e3401440f42d186
황준혁 <hyug0819@gmail.com>
2023-09-19 09:07:21 +0900

    [S09P22B308-293] feat : Color Repository 코드

--- backend/src/main/java/com/fintech/masoori/domain/lucky/repository/ColorRepository.java
+++ backend/src/main/java/com/fintech/masoori/domain/lucky/repository/ColorRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fintech.masoori.domain.lucky.entity.Color;
 
 public interface ColorRepository extends JpaRepository<Color, Long> {
+	public Color findDescriptionByColor(String color);
 }
