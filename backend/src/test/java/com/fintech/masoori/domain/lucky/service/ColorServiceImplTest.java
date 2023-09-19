3cdf59adfe305f89e01f6ecddacb381b73626b7b
황준혁 <hyug0819@gmail.com>
2023-09-19 10:48:25 +0900

    [S09P22B308-293] feat : 테스트코드 작성

--- backend/src/test/java/com/fintech/masoori/domain/lucky/service/ColorServiceImplTest.java
+++ backend/src/test/java/com/fintech/masoori/domain/lucky/service/ColorServiceImplTest.java
@@ -1,6 +1,6 @@
 package com.fintech.masoori.domain.lucky.service;
 
-import java.util.Optional;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +26,9 @@ class ColorServiceImplTest {
 	@Autowired
 	RedisService redisService;
 
+	/*
+	유저가 처음 색을 뽑았을 때
+	 */
 	@Test
 	void firstSelectColor() {
 		Color color1 = Color.builder().color("#111111").description("1번 색").build();
@@ -41,14 +44,44 @@ class ColorServiceImplTest {
 		colorRepository.save(color5);
 		colorRepository.save(color6);
 		User user = User.builder().email("test@gmail.com").password("123").build();
-		Optional<String> userColorOptional = Optional.ofNullable(redisService.getUserColor(user.getEmail()));
-		if (userColorOptional.isPresent()) {
-			log.info("Already user select color");
+		if (redisService.getUserColor(user.getEmail()) == null) {
+			//오늘의 색 하나 조회
 			ColorRes response = colorService.selectOneColor(user.getEmail());
-		} else {
-			ColorRes response = colorService.selectOneColor(user.getEmail());
-			log.info("Response : {}", response);
+			log.info("Response : {}, User : {}", response, user.getEmail());
+			assertThat(redisService.getUserColor(user.getEmail()).equals(response.getColor()));
+			//레디스에서 유저 제거
+			redisService.deleteUserColor(user.getEmail());
+			assertThat(redisService.getUserColor(user.getEmail()) == null);
 		}
+	}
 
+	/*
+	이미 색을 뽑은 유저가 다시 방문했을 때
+	 */
+	@Test
+	void alreadySelectColor() {
+		Color color1 = Color.builder().color("#111111").description("1번 색").build();
+		Color color2 = Color.builder().color("#222222").description("2번 색").build();
+		Color color3 = Color.builder().color("#333333").description("3번 색").build();
+		Color color4 = Color.builder().color("#444444").description("4번 색").build();
+		Color color5 = Color.builder().color("#555555").description("5번 색").build();
+		Color color6 = Color.builder().color("#666666").description("6번 색").build();
+		colorRepository.save(color1);
+		colorRepository.save(color2);
+		colorRepository.save(color3);
+		colorRepository.save(color4);
+		colorRepository.save(color5);
+		colorRepository.save(color6);
+		User user = User.builder().email("test@gmail.com").password("123").build();
+		//유저 방문
+		ColorRes response = colorService.selectOneColor(user.getEmail());
+		if (redisService.getUserColor(user.getEmail()).equals(response.getColor())) {
+			//redis에 있는 컬러가 저장된 컬러가 맞는지
+			String Color = redisService.getUserColor(user.getEmail());
+			log.info("Color : {}", colorRepository.findDescriptionByColor(Color));
+			assertThat(Color.equals(response.getColor()));
+		}
+		redisService.deleteUserColor(user.getEmail());
+		assertThat(redisService.getUserColor(user.getEmail()) == null);
 	}
 }
