ddeef856b93a91b49845fecc72285ef0d0af63a9
황준혁 <hyug0819@gmail.com>
2023-09-20 23:14:34 +0900

    [S09P22B308-297] refactor : 어노테이션 추가 및 멤버명 변경

--- backend/src/main/java/com/fintech/masoori/domain/card/dto/BasicCardRes.java
+++ backend/src/main/java/com/fintech/masoori/domain/card/dto/BasicCardRes.java
@@ -7,9 +7,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Builder
+@Getter
 public class BasicCardRes {
 
 	@Builder.Default
@@ -17,14 +19,14 @@ public class BasicCardRes {
 
 	@Data
 	@Builder
+	@Getter
 	@AllArgsConstructor
 	public static class BasicCard {
 		@Schema(description = "카드 기본 정보")
 		private Card card;
 		@Schema(description = "카드에 속한 소비정보")
 		@Builder.Default
-		private List<Basic> basics = new ArrayList<>();
-
+		private List<Basic> basicList = new ArrayList<>();
 	}
 
 }
--- backend/src/main/java/com/fintech/masoori/domain/card/dto/ChallengeCardRes.java
+++ backend/src/main/java/com/fintech/masoori/domain/card/dto/ChallengeCardRes.java
@@ -7,21 +7,24 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Builder
+@Getter
 public class ChallengeCardRes {
 	@Builder.Default
 	private List<ChallengeCard> challengeCardList = new ArrayList<>();
 
 	@Data
 	@Builder
+	@Getter
 	@AllArgsConstructor
 	public static class ChallengeCard {
 		@Schema(description = "카드 기본 정보")
 		private Card card;
 		@Schema(description = "카드에 속한 챌린지들")
 		@Builder.Default
-		private List<Challenge> challenges = new ArrayList<>();
+		private List<Challenge> challengeList = new ArrayList<>();
 	}
 }
