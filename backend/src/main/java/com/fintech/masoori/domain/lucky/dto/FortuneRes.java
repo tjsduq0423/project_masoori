36fdb5c008a61b501bf4222d223ba92c99adad36
송소연 <songsoy95@gmail.com>
2023-09-22 13:35:59 +0900

    [S09P12B308-234] fix : 금전운Entity변경

--- backend/src/main/java/com/fintech/masoori/domain/lucky/dto/FortuneRes.java
+++ backend/src/main/java/com/fintech/masoori/domain/lucky/dto/FortuneRes.java
@@ -23,6 +23,8 @@ public class FortuneRes {
 		private String name;
 		@Schema(description = "금전운 이미지 경로", example = "j9b308.p.ssafy.io/img/1234.png")
 		private String imagePath;
+		@Schema(description = "마녀의 한마디", example = "생각지도 못했던 곳에 갑자기 돈을 쓸 수 있어요.")
+		private String summary;
 		@Schema(description = "금전운 설명", example = "이 카드를 뽑은 당신! 오늘은...")
 		private String description;
 
