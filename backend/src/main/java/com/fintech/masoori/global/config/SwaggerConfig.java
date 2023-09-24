3e185138adadc0481a5caf4076f701daef531aff
황준혁 <hyug0819@gmail.com>
2023-09-25 00:30:42 +0900

    [S09P22B308-288] feat : Authorization 추가

--- backend/src/main/java/com/fintech/masoori/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/fintech/masoori/global/config/SwaggerConfig.java
@@ -1,17 +1,28 @@
 package com.fintech.masoori.global.config;
 
+import java.util.Arrays;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+  SecurityScheme securityScheme = new SecurityScheme()
+      .type(SecurityScheme.Type.HTTP).scheme("Bearer").bearerFormat("JWT")
+      .in(SecurityScheme.In.HEADER).name("Authorization");
+  SecurityRequirement securityRequirement = new SecurityRequirement().addList("Bearer");
+
   @Bean
   public OpenAPI openAPI() {
     return new OpenAPI()
-        .components(new Components())
+        .components(new Components().addSecuritySchemes("Bearer", securityScheme))
+        .security(Arrays.asList(securityRequirement))
         .info(apiInfo());
   }
   private Info apiInfo() {
