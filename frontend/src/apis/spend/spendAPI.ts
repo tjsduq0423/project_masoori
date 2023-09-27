fec4264f50e29c8ef4e23f17d927db8291a61583
cje9717@naver.com <cje9717@naver.com>
2023-09-27 15:33:58 +0900

    [S09P22B308-382] fix : api 수정

--- frontend/src/apis/spend/spendAPI.ts
+++ frontend/src/apis/spend/spendAPI.ts
@@ -1,15 +1,5 @@
 import { instance } from "@/apis/instance";
 
-// 사용자 최초 등록 시 소비카드를 생성한다.
-const postConsume = async () => {
-  try {
-    const response = await instance.post(`/card/consume`);
-    return response.data;
-  } catch {
-    new Error("api 연동 오류 - postConsume");
-  }
-};
-
 const postGhost = async () => {
   try {
     const response = await instance.post(`/user/ghost`);
@@ -19,4 +9,13 @@ const postGhost = async () => {
   }
 };
 
-export { postConsume, postGhost };
+const getConsumeId = async (id: number) => {
+  try {
+    const response = await instance.get(`/card/consume/${id}`);
+    return response.data;
+  } catch {
+    new Error("api 연동 오류 - getConsumeId");
+  }
+};
+
+export { postGhost, getConsumeId };
