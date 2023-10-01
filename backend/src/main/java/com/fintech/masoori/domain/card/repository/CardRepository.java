39c18951136c8b653d84a89d7972e2c1508aeaaa
황준혁 <hyug0819@gmail.com>
2023-10-01 20:50:47 +0900

    [S09P22B308-352] feat : selectUserLastBasicCard 관련 기능 구현

--- backend/src/main/java/com/fintech/masoori/domain/card/repository/CardRepository.java
+++ backend/src/main/java/com/fintech/masoori/domain/card/repository/CardRepository.java
@@ -3,6 +3,7 @@ package com.fintech.masoori.domain.card.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,4 +19,7 @@ public interface CardRepository extends JpaRepository<Card, Long> {
 
 	@Query("SELECT c FROM Card c WHERE c.user.id = :userId AND c.cardType = :type AND c.id = :cardId")
 	Card findCard(@Param("userId") long userId, @Param("type") CardType type, @Param("cardId") long cardId);
+
+	@Query("SELECT c FROM Card c WHERE c.user.id = :userId AND c.cardType = :type AND c.createdDate >= :startDate AND c.createdDate <= :endDate ")
+	Card findRecentCard(@Param("userId") long userId, @Param("type") CardType type, @Param("startDate")LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
