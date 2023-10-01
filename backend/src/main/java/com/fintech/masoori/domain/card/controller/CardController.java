487d4785ed833f2bd10bfbb0fb12786ea47ba5d5
황준혁 <hyug0819@gmail.com>
2023-10-01 22:12:29 +0900

    [S09P22B308-000] refactor : 최근카드조회 Swagger 수정

--- backend/src/main/java/com/fintech/masoori/domain/card/controller/CardController.java
+++ backend/src/main/java/com/fintech/masoori/domain/card/controller/CardController.java
@@ -86,9 +86,10 @@ public class CardController {
 	}
 
 	//이번주 챌린지 카드 조회
-	@Operation(summary = "해당 주차 사용자 소비카드 조회 API", description = "사용자가 이번주에 소비카드를 만들었는지, 만들지 않았는지 판단한다. \n 이번주에 만들어진 카드가 없다면 400(data exist) \n 카드가 만들어지고 있다면(테이블에 id는 존재하는데 값들이 없는 경우) 406(data creating)")
+	@Operation(summary = "해당 주차 사용자 소비카드 조회 API", description = "사용자가 이번주에 소비카드를 만들었는지, 만들지 않았는지 판단한다. 1.최초 사용자, 이번주에 만들어진 카드가 없는 사용자 : 무조건 카드가 없기 때문에 200(이번주 생성 가능)에러 발생 2.이번주에 카드를 이미 만든 사용자 : 이번주에 만든 카드 정보가 그대로 반환 3.카드가 만들어지고 있는 사용자 : 400에러 발생")
 	@GetMapping("/consume/recent")
-	public ResponseEntity<BasicCardRes.BasicCard> selectUserLastBasicCard(@Parameter(description = "현재 시간", example = "2023-09-26T07:42:34.76") LocalDateTime now, Principal principal) {
+	public ResponseEntity<BasicCardRes.BasicCard> selectUserLastBasicCard(@Parameter(name = "now", description = "현재 시간", example = "2023-09-26T07:42:34.76") LocalDateTime now, Principal principal) {
+		System.out.println("NOW : "+now);
 		BasicCardRes.BasicCard basicCard = cardService.selectUserRecentBasicCard(principal.getName(), now);
 		//카드를 만들 수 있는 사용자
 		if(basicCard == null){
