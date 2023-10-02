d3007799866e0cf37a9a3592ce120808a1a834da
cje9717@naver.com <cje9717@naver.com>
2023-10-02 21:15:40 +0900

    [S09P22B308-171] remove : 파일 삭제

--- frontend/src/apis/spend/Queris/useGetConsumeId.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getConsumeId } from "@/apis/spend/spendAPI";
-
-const useGetConsumeId = (cardId: number) => {
-  const { data } = useQuery(["consumeId", cardId], () => getConsumeId(cardId));
-  return data;
-};
-
-export { useGetConsumeId };
