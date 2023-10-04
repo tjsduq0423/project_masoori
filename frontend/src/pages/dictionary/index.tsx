a96fc7971af106b0d08fcf879a3d8874201b06df
Ssuhyeong <tngud124@gmail.com>
2023-10-04 16:24:39 +0900

    [S09P22B308-442] feat : 도감 부분 수정

--- frontend/src/pages/dictionary/index.tsx
+++ frontend/src/pages/dictionary/index.tsx
@@ -19,6 +19,7 @@ import {
 import { useNavigate } from "react-router-dom";
 import { useProfileImage } from "@/apis/menu/Mutations/useProfileImage";
 import { toast } from "react-toastify";
+import Lottie from "lottie-react";
 
 import cardBack from "@/assets/img/tarotCard/tarotCardBack.png";
 
@@ -222,7 +223,7 @@ const DictionaryPage = () => {
     try {
       // 이름과 전화번호를 사용하여 SMS를 보냅니다.
       await profileImage.mutateAsync(challengeCard.card.id);
-      toast.info("🃏 프로필 카드 등록완료 🃏");
+      toast.info("🃏 프로필 카드 등록 완료 🃏");
     } catch (error) {
       console.error("인증 코드 전송 실패:", error);
     }
