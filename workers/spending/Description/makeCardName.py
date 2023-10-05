c0bd0c7ae6eb681151a6ef13379706e5f386f02c
황준혁 <hyug0819@gmail.com>
2023-10-06 03:47:43 +0900

    [S09P22B308-000] refactor : gpt 설정 변경
    Former-commit-id: 64936c5927850a9d8278d4ab7c8b3d509a3a5d4e

--- workers/spending/Description/makeCardName.py
+++ workers/spending/Description/makeCardName.py
@@ -4,13 +4,13 @@ from langchain.chains import LLMChain
 
 def MakeCardName(spendData):
     try:
-        chat = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.7, verbose=True, max_tokens=2048, max_retries=3)
+        chat = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.3, verbose=True, max_tokens=2048, max_retries=3)
         prompt = PromptTemplate(
             input_variables=["Data"],
             template="""
             You're a nomenclist.
 
-            Please recommend a name that doesn't exceed 15 characters in English for user's input of "frequencyKeyword" or "amountKeyword"
+            Please recommend a name that doesn't exceed 15 characters in English for user's input of "amountKeyword"
 
             conversation:
             user input : {Data}
