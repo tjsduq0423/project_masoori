4713b57fb3f91ed5db6a2a465f961e7537044feb
문준호 <tjsduq0423@naver.com>
2023-10-06 05:39:51 +0900

    [S09P22B308-000] feat : card ID 중복 수정
    Former-commit-id: 34569b398c8fec73d2461aab9fba3a84eb84ebcf

--- backend/src/main/java/com/fintech/masoori/global/config/RabbitMQConfig.java
+++ backend/src/main/java/com/fintech/masoori/global/config/RabbitMQConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
