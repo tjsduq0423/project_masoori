2fe8e16e89e4956b2dee5049fd021ae7bcc9726e
황준혁 <hyug0819@gmail.com>
2023-10-06 09:36:11 +0900

    [S09P22B308-000] refactor : Card 변경
    Former-commit-id: c241f620cc189855e908a6d9db5ea4130a5890cc

--- backend/src/main/java/com/fintech/masoori/domain/card/repository/CardRepository.java
+++ backend/src/main/java/com/fintech/masoori/domain/card/repository/CardRepository.java
@@ -25,6 +25,9 @@ public interface CardRepository extends JpaRepository<Card, Long> {
 	@Query("SELECT c FROM Card c WHERE c.user.id = :userId AND c.cardType = :type AND c.createdDate >= :startDate AND c.createdDate <= :endDate ")
 	Card findRecentCard(@Param("userId") long userId, @Param("type") CardType type, @Param("startDate")LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
+	@Query("SELECT c FROM Card c WHERE c.id = :cardId AND c.cardType = :type AND c.createdDate >= :startDate AND c.createdDate <= :endDate")
+	Card findCardByCardId(@Param("cardId") long cardId, @Param("type") CardType type, @Param("startDate")LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+
 	Card findCardByUserIdAndId(Long userId, Long id);
 
 	@Query("SELECT c FROM Card c WHERE c.user.id = :userId AND c.cardType = :type AND c.createdDate <= :now ORDER BY c.createdDate DESC")
